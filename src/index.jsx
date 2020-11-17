4fea200f6a6a42302de7276fdc52473c33800107
Jana Stastna <j.machkova@gmail.com)>
2020-11-17 20:21:08 +0100

    opening

--- src/index.jsx
+++ src/index.jsx
@@ -14,13 +14,22 @@ render(
           <a href="#">O projektu</a>
           <a href="#">Chci darovat</a>
           <a href="#">Potřebuji počítač</a>
+          <a href="#">Kontakt</a>
         </div>
+      </div>
+      <div className="opening">
+        <p className="opening__par1">Počítače pro školáky</p>
+        <p className="opening__par2">
+          Nedarujete jen počítač, darujete vzdělání!
+        </p>
       </div>
     </header>
     <main>
-      <p>Moje první React stránka</p>
+      <div className="for"></div>
+      <div className="for__donors"></div>
+      <div className="for__schools"></div>
     </main>
-    <footer>Martin Podloucký</footer>
+    <footer>Czechitas</footer>
   </>,
   document.querySelector('#app'),
 );
