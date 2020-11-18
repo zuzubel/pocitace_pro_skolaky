43264481514832b2342cc729badc724c38800fa0
Jana Stastna <j.machkova@gmail.com)>
2020-11-18 22:00:38 +0100

    about project

--- src/components/AboutProject/index.jsx
+++ src/components/AboutProject/index.jsx
@@ -5,7 +5,14 @@ export const AboutProject = () => {
   return (
     <>
       <div className="aboutProject">
-        <h1>Zkouška</h1>
+        <h1 className="aboutProject--h">O projektu</h1>
+        <p className="aboutProject--p">
+          Pandemie koronaviru přenusula studium do online prostředí. Mnoho žáků
+          a studentů ale nemá vyhovující podmínky a každým dnem jim tak utíká
+          část výuky. Tuto zrátu budou po návratu do škol stěží dohánět. Pojďme
+          společně pomoci! Dostaňme nepotřebnou IT techniku ke studentům, kteří
+          ji potřebují.
+        </p>
       </div>
     </>
   );
