0e514174d218fbbafde9f8f35021ac59836dd6b1
Jana Stastna <j.machkova@gmail.com)>
2020-11-19 14:37:30 +0100

    about project foto

--- src/components/AboutProject/index.jsx
+++ src/components/AboutProject/index.jsx
@@ -5,14 +5,17 @@ export const AboutProject = () => {
   return (
     <>
       <div className="aboutProject">
-        <h1 className="aboutProject--h">O projektu</h1>
-        <p className="aboutProject--p">
-          Pandemie koronaviru přenusula studium do online prostředí. Mnoho žáků
-          a studentů ale nemá vyhovující podmínky a každým dnem jim tak utíká
-          část výuky. Tuto zrátu budou po návratu do škol stěží dohánět. Pojďme
-          společně pomoci! Dostaňme nepotřebnou IT techniku ke studentům, kteří
-          ji potřebují.
-        </p>
+        <div className="aboutProject--text">
+          <h1 className="aboutProject--h">O projektu</h1>
+          <p className="aboutProject--p">
+            Pandemie koronaviru přenusula studium do online prostředí. Mnoho
+            žáků a studentů ale nemá vyhovující podmínky a každým dnem jim tak
+            utíká část výuky. Tuto zrátu budou po návratu do škol stěží dohánět.
+            Pojďme společně pomoci! Dostaňme nepotřebnou IT techniku ke
+            studentům, kteří ji potřebují.
+          </p>
+        </div>
+        <div className="aboutProject--foto"></div>
       </div>
     </>
   );
