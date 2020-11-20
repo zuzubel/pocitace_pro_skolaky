13f04e63aeafc3b0d8b79edd62bf6e542acc0d86
Zuzana Belaskova <z.belaskova@gmail.com>
2020-11-20 12:28:03 +0100

    Section 'how it works' created. Nav menu sticked to the top for mobiles

---
+++ src/components/HowItWorks/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './style.css'
+
+export const HowItWorks = () => {
+  return (
+    <div className="howItWorks__div">
+      <div className="howItWorks__div-donate">
+        <h3>Jak mohu darovat počítač?</h3>
+      </div>
+      <div className='howItWorks__div-apply'>
+        <h3>Jak mohu získat počítač?</h3>
+      </div>
+    </div>
+  )
+}
