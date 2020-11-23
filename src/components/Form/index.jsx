0cafdef08f7ae3e31b44839ff31e46f1b27ef55c
Jana Stastna <j.machkova@gmail.com)>
2020-11-23 22:19:11 +0100

    form_firebase_dalsi verze

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -110,6 +110,15 @@ export const Form = () => {
       <ul>
         {polozky.map((polozka, i) => (
           <li key={i}>
+            <input
+              type="checkbox"
+              checked={polozka.vyrizeno}
+              onChange={(event) => {
+                db.collection('chci_pocitac').doc(polozka.id).update({
+                  vyrizeno: event.target.checked,
+                });
+              }}
+            />
             {
               (polozka.skola,
               polozka.skolaAdresa,
@@ -119,9 +128,22 @@ export const Form = () => {
               polozka.poptavka,
               polozka.info)
             }
+            <button
+              onClick={() => {
+                db.collection('chci_pocitac').doc(polozka.id).delete();
+              }}
+            >
+              SMAZAT INZERÁT
+            </button>
           </li>
         ))}
-        <input
+      </ul>
+    </>
+  );
+};
+
+/*
+<input
           type="checkbox"
           checked={polozka.vyrizeno}
           onChange={(event) => {
@@ -130,14 +152,5 @@ export const Form = () => {
             });
           }}
         />
-      </ul>
-      <button
-        onClick={() => {
-          db.collection('chci_pocitac').doc(polozka.id).delete();
-        }}
-      >
-        SMAZAT
-      </button>
-    </>
-  );
-};
+
+        */
