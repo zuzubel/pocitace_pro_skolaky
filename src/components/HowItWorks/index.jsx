8b9f12591325ce35ae7f4d55f9b2521c82b248f4
Zuzana Belaskova <z.belaskova@gmail.com>
2020-11-23 15:10:15 +0100

    styling details

--- src/components/HowItWorks/index.jsx
+++ src/components/HowItWorks/index.jsx
@@ -3,11 +3,11 @@ import './style.css'
 
 export const HowItWorks = () => {
   return (
-    <div className="howItWorks__div">
-      <div className="howItWorks__div-donate">
+    <div className="howItWorks__container">
+      <div className="howItWorks__donate">
         <h3>Jak mohu darovat počítač?</h3>
       </div>
-      <div className='howItWorks__div-apply'>
+      <div className='howItWorks__apply'>
         <h3>Jak mohu získat počítač?</h3>
       </div>
     </div>
