013feab973a4b33c76e69b05eec753e14faf812d
Jana Stastna <j.machkova@gmail.com)>
2020-11-24 13:48:34 +0100

    form css

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import './style.css';
 import { db } from '../../db.js';
 import firebase from 'firebase/app';
 
@@ -27,153 +28,148 @@ export const Form = () => {
       });
   }, []);
 
-  /*
-  {polozky.map((polozka) => (
-    <li key={polozka.id}>
-      <input
-        type="checkbox"
-        checked={polozka.koupeno}
-        onChange={(event) => {
-          databaze.collection('seznam').doc(polozka.id).update({
-            koupeno: event.target.checked,
-          })
-        }}
-      />
-      {polozka.nazev}{' '}
-      <button
-        onClick={() => {
-          databaze.collection('seznam').doc(polozka.id).delete()
-        }}
-      >
-        smazat
-      </button>
-    </li>
-  ))}
-</ul>
-*/
-
   return (
     <>
-      <h1>Formulář</h1>
-      <h2>Co nám formulář vypisuje: </h2>
-      <ul>
-        {polozky.map((polozka) => (
-          <>
-            <li key={polozka.id}>
-              <input
-                type="checkbox"
-                checked={polozka.vyrizeno}
-                onChange={(event) => {
-                  db.collection('chci_pocitac').doc(polozka.id).update({
-                    vyrizeno: event.target.checked,
-                  });
-                }}
-              />
-              <button
-                onClick={() => {
-                  db.collection('chci_pocitac').doc(polozka.id).delete();
-                }}
-              >
-                SMAZAT INZERÁT
-              </button>
-            </li>
-            <li>{polozka.skola}</li>
-            <li>{polozka.skola_adresa}</li>
-            <li>{polozka.poptavam}</li>
-            <li>{polozka.info}</li>
-            <li>{polozka.kontakt_email}</li>
-            <li>{polozka.kontakt_telefon}</li>
-          </>
-        ))}
-      </ul>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          db.collection('chci_pocitac').add({
-            skola: skola,
-            skola_adresa: skolaAdresa,
-            kontaktni_osoba: kontakt,
-            kontakt_email: kontaktEmail,
-            kontakt_telefon: kontaktTelefon,
-            poptavam: poptavka,
-            info: info,
-            vyrizeno: false,
-            datumVytvoreni: firebase.firestore.FieldValue.serverTimestamp(),
-          });
-          setSkola(''),
-            setSkolaAdresa(''),
-            setKontakt(''),
-            setKontaktEmail(''),
-            setKontaktTelefon(''),
-            setPoptavka(''),
-            setInfo('');
-        }}
-      >
-        <label>
-          Název školy:{' '}
-          <input
-            value={skola}
-            onChange={(event) => setSkola(event.target.value)}
-          />
-        </label>
-        <label>
-          Adresa:{' '}
-          <input
-            value={skolaAdresa}
-            onChange={(event) => setSkolaAdresa(event.target.value)}
-          />
-        </label>
-
-        <label>
-          Kontaktní osoba:{' '}
-          <input
-            value={kontakt}
-            onChange={(event) => setKontakt(event.target.value)}
-          />
-        </label>
-        <label>
-          E-mail:{' '}
-          <input
-            value={kontaktEmail}
-            onChange={(event) => setKontaktEmail(event.target.value)}
-          />
-        </label>
-        <label>
-          Telefon:{' '}
-          <input
-            value={kontaktTelefon}
-            onChange={(event) => setKontaktTelefon(event.target.value)}
-          />
-        </label>
-        <label>
-          Co potřebuji:{' '}
-          <input
-            value={poptavka}
-            onChange={(event) => setPoptavka(event.target.value)}
-          />
-        </label>
-        <label>
-          Doplňující informace:{' '}
-          <input
-            value={info}
-            onChange={(event) => setInfo(event.target.value)}
-          />
-        </label>
-        <button>Přidat poptávku</button>
-      </form>
+      <section className="form_sec">
+        <div className="form_div">
+          <h1 className="form_headline">Formulář</h1>
+          <ul>
+            {polozky.map((polozka) => (
+              <>
+                <li key={polozka.id}>
+                  <input
+                    type="checkbox"
+                    checked={polozka.vyrizeno}
+                    onChange={(event) => {
+                      db.collection('chci_pocitac').doc(polozka.id).update({
+                        vyrizeno: event.target.checked,
+                      });
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      db.collection('chci_pocitac').doc(polozka.id).delete();
+                    }}
+                  >
+                    SMAZAT INZERÁT
+                  </button>
+                </li>
+                <li>{polozka.skola}</li>
+                <li>{polozka.skola_adresa}</li>
+                <li>{polozka.poptavam}</li>
+                <li>{polozka.info}</li>
+                <li>{polozka.kontakt_email}</li>
+                <li>{polozka.kontakt_telefon}</li>
+              </>
+            ))}
+          </ul>
+          <form
+            className="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              db.collection('chci_pocitac').add({
+                skola: skola,
+                skola_adresa: skolaAdresa,
+                kontaktni_osoba: kontakt,
+                kontakt_email: kontaktEmail,
+                kontakt_telefon: kontaktTelefon,
+                poptavam: poptavka,
+                info: info,
+                vyrizeno: false,
+                datumVytvoreni: firebase.firestore.FieldValue.serverTimestamp(),
+              });
+              setSkola(''),
+                setSkolaAdresa(''),
+                setKontakt(''),
+                setKontaktEmail(''),
+                setKontaktTelefon(''),
+                setPoptavka(''),
+                setInfo('');
+            }}
+          >
+            <div className="form__inputs">
+              <div className="row">
+                <label>
+                  Název školy:{' '}
+                  <input
+                    type="text"
+                    value={skola}
+                    onChange={(event) => setSkola(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Adresa:{' '}
+                  <input
+                    type="text"
+                    value={skolaAdresa}
+                    onChange={(event) => setSkolaAdresa(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Kontaktní osoba:{' '}
+                  <input
+                    type="text"
+                    value={kontakt}
+                    onChange={(event) => setKontakt(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  E-mail:{' '}
+                  <input
+                    type="email"
+                    value={kontaktEmail}
+                    onChange={(event) => setKontaktEmail(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Telefon:{' '}
+                  <input
+                    type="tel"
+                    value={kontaktTelefon}
+                    onChange={(event) => setKontaktTelefon(event.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Co potřebuji:{' '}
+                  <input
+                    type="text"
+                    value={poptavka}
+                    onChange={(event) => setPoptavka(event.target.value)}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className="form__textarea">
+              <div className="row">
+                <label>
+                  Doplňující informace:{' '}
+                  <textarea
+                    type="text"
+                    placeholder="Dejte nám dopňující informace a zvyšte tak šanci, že právě vaše přání bude vyslyšeno."
+                    value={info}
+                    onChange={(event) => setInfo(event.target.value)}
+                  />
+                </label>
+              </div>
+            </div>
+            <button>Přidat poptávku</button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
 
-/*
-<input
-          type="checkbox"
-          checked={polozka.vyrizeno}
-          onChange={(event) => {
-            db.collection('chci_pocitac').doc(polozka.id).update({
-              vyrizeno: event.target.checked,
-            });
-          }}
-        />
-
-        */
+//doplňující informace: udělat větší okénko
+//telefon: nastavit jako číslo
+//přidat povinné položky
