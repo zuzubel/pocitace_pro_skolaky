86b29c82e3e1b7d55eff1f6494ce18039c851163
Jana Stastna <j.machkova@gmail.com)>
2020-11-27 18:34:18 +0100

    donate_css

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -5,9 +5,19 @@ export const Donate = () => {
   return (
     <div className="donate__containe">
       <div className="donate__opening">
-<p className="donate__opening__h">Něco pěkného</p>
-<p className="donate__opening__p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque quae obcaecati nam laborum at, fugit aperiam nemo totam soluta ab fugiat voluptate praesentium sint eligendi eaque accusamus distinctio iure quisquam sunt natus rem iste temporibus hic velit? Dolores maiores voluptatum quod?</p>
+        <p className="donate__opening__h">Dejte svému počítači druhou šanci</p>
+        <p className="donate__opening__p">
+          Válí se vám doma ve sklepě zánovní, ale stále funkční, počítač?
+          Dostali jste od Ježíška nový notebook a marně přemýšlíte, co s tím
+          starým? Nebo v práci pravidelně obnovujete IT techniku a starší
+          počítače už nemají využití? Máme pro vás řešení! Vdechněte vašim
+          nevyužitým počítačům druhý dech a nabídněte je dál žákům a studentům,
+          kterým usnadníte jejich vzdělávání.{' '}
+        </p>
+        <a className="donate__opening__link" href="">
+          Podívejte se, kdo shání počítač.
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
