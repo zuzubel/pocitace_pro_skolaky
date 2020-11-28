8090c917c62c62c4a4156edbe2ab1a07e8d1ee21
Zuzana Belaskova <z.belaskova@gmail.com>
2020-11-28 11:29:34 +0100

    links added to text and icons

--- src/components/For/index.jsx
+++ src/components/For/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export const For = () => {
   return (
     <div className="for">
       <div className="for__donors">
-        <div className="for__donors--foto"></div>
-        <a className="for__donors--a" href="#">
+        <Link to="/donate" className="for__donors--foto"><div ></div></Link>
+        <Link className="for__donors--a" to="/donate">
           Chci pomoci
-        </a>
+        </Link>
       </div>
       <div className="for__schools">
-        <div className="for__schools--foto"></div>
-        <a className="for__schools--a" href="#">
+        <Link to="/form" className="for__schools--foto"><div ></div></Link>
+        <Link className="for__schools--a" to="/form">
           Potřebuji pomoc
-        </a>
+        </Link>
       </div>
     </div>
   );
