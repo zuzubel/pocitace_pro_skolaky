8f34f338331b610e57d9b2f2bc3740d9fc361c25
Jana Stastna <j.machkova@gmail.com)>
2020-11-29 15:18:16 +0100

    form FINAL

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -12,7 +12,7 @@ export const Form = () => {
   const [contactEmail, setContactEmail] = useState('a@gmail.com');
   const [contactTelephone, setContactTelephone] = useState('123123123');
   const [request, setRequest] = useState('a');
-  const [info, setInfo] = useState('a');
+  const [info, setInfo] = useState('');
   const [agreement, setAgreement] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [region, setRegion] = useState('');
@@ -33,165 +33,171 @@ export const Form = () => {
         </p>
         <p className="form__opening__f">Dejme dětem šanci studovat!</p>
       </div>
-      <div className="form_div">
-        <h1 className="form_headline">Formulář</h1>
+      <section className="form">
+        <div className="form_div">
+          <h1 className="form_headline">Formulář</h1>
 
-        <form
-          className="form_body"
-          onSubmit={(event) => {
-            event.preventDefault();
-            db.collection('chci_pocitac').add({
-              school: school,
-              school_adress: schoolAdress,
-              region: region,
-              contact_person: contact,
-              contact_email: contactEmail,
-              contact_telephone: contactTelephone,
-              request: request,
-              info: info,
-              agree: agreement,
-              state: false,
-              date: firebase.firestore.FieldValue.serverTimestamp(),
-            });
-            setSchool(''),
-              setSchoolAdress(''),
-              setContact(''),
-              setContactEmail(''),
-              setContactTelephone(''),
-              setRequest(''),
-              setInfo('');
-            setRegion('');
-            setAgreement(false);
-            setSubmitted(true);
-          }}
-        >
-          <div className="form__inputs">
-            <div className="row">
-              <label>
-                Název školy:{' '}
-                <input
-                  type="text"
-                  value={school}
-                  onChange={(event) => setSchool(event.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div className="row">
-              <label>
-                Adresa školy:{' '}
-                <input
-                  type="text"
-                  value={schoolAdress}
-                  onChange={(event) => setSchoolAdress(event.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div className="div__select">
-              <label className="form__select__label">
-                Vyberte kraj:{' '}
-                <select
-                  value={region}
-                  onChange={(e) => setRegion(e.target.value)}
-                  className="form__select"
-                  required
-                >
-                  {regions.map((region) => (
-                    <option value={region.value}>{region.label}</option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <div className="row">
-              <label>
-                Kontaktní osoba:{' '}
-                <input
-                  type="text"
-                  value={contact}
-                  onChange={(event) => setContact(event.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div className="row">
-              <label>
-                E-mail:{' '}
-                <input
-                  type="email"
-                  value={contactEmail}
-                  onChange={(event) => setContactEmail(event.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div className="row">
-              <label>
-                Telefon:{' '}
-                <input
-                  title="Pro zadání telefonního čísla, můžete využít tyto formáty: +123 123 123 123, 00123123123, 123123123, 123 123 123"
-                  type="tel"
-                  pattern="[+]?[()/0-9. -]{9,}"
-                  value={contactTelephone}
-                  onChange={(event) => setContactTelephone(event.target.value)}
-                />
-              </label>
-            </div>
-            <div className="row">
-              <label>
-                Poptávám:{' '}
-                <input
-                  type="text"
-                  value={request}
-                  onChange={(event) => setRequest(event.target.value)}
-                  required
-                />
-              </label>
-            </div>
-            <div className="form__textarea">
+          <form
+            className="form_body"
+            onSubmit={(event) => {
+              event.preventDefault();
+              db.collection('chci_pocitac').add({
+                school: school,
+                school_adress: schoolAdress,
+                region: region,
+                contact_person: contact,
+                contact_email: contactEmail,
+                contact_telephone: contactTelephone,
+                request: request,
+                info: info,
+                agree: agreement,
+                state: false,
+                date: firebase.firestore.FieldValue.serverTimestamp(),
+              });
+              setSchool(''),
+                setSchoolAdress(''),
+                setContact(''),
+                setContactEmail(''),
+                setContactTelephone(''),
+                setRequest(''),
+                setInfo('');
+              setRegion('');
+              setAgreement(false);
+              setSubmitted(true);
+            }}
+          >
+            <div className="form__inputs">
+              <div className="form__inputs--flex">
               <div className="row">
                 <label>
-                  <textarea
+                  Název školy:{' '}
+                  <input
                     type="text"
-                    placeholder="Zde vložte dopňující informace a zvyšte tak šanci, že právě vaše přání bude vyslyšeno."
-                    value={info}
-                    onChange={(event) => setInfo(event.target.value)}
+                    value={school}
+                    onChange={(event) => setSchool(event.target.value)}
+                    required
                   />
                 </label>
               </div>
+              <div className="row">
+                <label>
+                  Adresa školy:{' '}
+                  <input
+                    type="text"
+                    value={schoolAdress}
+                    onChange={(event) => setSchoolAdress(event.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="div__select">
+                <label className="form__select__label">
+                  Vyberte kraj:{' '}
+                  <select
+                    value={region}
+                    onChange={(e) => setRegion(e.target.value)}
+                    className="form__select"
+                    required
+                  >
+                    {regions.map((region) => (
+                      <option value={region.value}>{region.label}</option>
+                    ))}
+                  </select>
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Kontaktní osoba:{' '}
+                  <input
+                    type="text"
+                    value={contact}
+                    onChange={(event) => setContact(event.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  E-mail:{' '}
+                  <input
+                    type="email"
+                    value={contactEmail}
+                    onChange={(event) => setContactEmail(event.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Telefon:{' '}
+                  <input
+                    title="Pro zadání telefonního čísla, můžete využít tyto formáty: +123 123 123 123, 00123123123, 123123123, 123 123 123"
+                    type="tel"
+                    pattern="[+]?[()/0-9. -]{9,}"
+                    value={contactTelephone}
+                    onChange={(event) =>
+                      setContactTelephone(event.target.value)
+                    }
+                  />
+                </label>
+              </div>
+              <div className="row">
+                <label>
+                  Poptávám:{' '}
+                  <input
+                    type="text"
+                    value={request}
+                    onChange={(event) => setRequest(event.target.value)}
+                    required
+                  />
+                </label>
+              </div>
+              </div>
+              <div className="form__textarea">
+                <div className="row">
+                  <label>
+                    <textarea
+                      type="text"
+                      placeholder="Zde vložte dopňující informace a zvyšte tak šanci, že právě vaše přání bude vyslyšeno."
+                      value={info}
+                      onChange={(event) => setInfo(event.target.value)}
+                    />
+                  </label>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="form__checkbox--p">
-            <div className="form__checkbox">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={agreement}
-                  onChange={() => {
-                    setAgreement(!agreement);
-                  }}
-                  required
-                />{' '}
-                * Souhlasím se zveřejněním kontaktních údajů.
-              </label>
+            <div className="form__checkbox--p">
+              <div className="form__checkbox">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={agreement}
+                    onChange={() => {
+                      setAgreement(!agreement);
+                    }}
+                    required
+                  />{' '}
+                  * Souhlasím se zveřejněním kontaktních údajů.
+                </label>
+              </div>
             </div>
-          </div>
 
-          <button type="submit" className="btn">
-            Přidat poptávku
-          </button>
-        </form>
-        {submitted ? (
-          <p className="submitted__p">
-            Hotovo! Vámi vložené údaje byly zpracovány ve výsledný inzerát,
-            který nyní čeká v sekci{' '}
-            <Link className="submitted__link" to="/donate">
-              Chci pomoci
-            </Link>{' '}
-            na dárce.
-          </p>
-        ) : null}
-      </div>
+            <button type="submit" className="btn">
+              Přidat poptávku
+            </button>
+          </form>
+          {submitted ? (
+            <p className="submitted__p">
+              Hotovo! Vámi vložené údaje byly zpracovány ve výsledný inzerát,
+              který nyní čeká v sekci{' '}
+              <Link className="submitted__link" to="/donate">
+                Chci pomoci
+              </Link>{' '}
+              na dárce.
+            </p>
+          ) : null}
+        </div>
+      </section>
     </>
   );
 };
